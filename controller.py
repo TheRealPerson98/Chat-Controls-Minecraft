99f4cc6b8d1f4bff3a9615e1fc81b031933797bc
Person98 <jacesleeman603@gmail.com>
2023-08-14 15:52:57 -0400

    Cleaned up controller

--- controller.py
+++ controller.py
@@ -1,24 +1,84 @@
 import time
 import pyautogui
 from colorama import init, Fore, Back, Style
-
-
-# Hello
+import pygetwindow as gw
 
 class Controller:
     ALLOWED_MESSAGES = [
         "w", "a", "s", "d", "space", "attack", "place", "mine",
         "e", "toggle sprint", "f3", "f2", "1", "2", "3", "4",
-        "5", "6", "7", "8", "9", "turn left", "turn right", "turn up", "turn down", "open"
+        "5", "6", "7", "8", "9", "turn left", "turn right", "turn up", "turn down", "open",
+        "dance"
     ]
+
+    KEY_MAPPINGS = {
+        "w": 'w',
+        "a": 'a',
+        "s": 's',
+        "d": 'd',
+        "space": 'space',
+        "attack": ',',
+        "open": '.',
+        "place": '.',
+        "mine": ',',
+        "e": 'e',
+        "toggle sprint": 'shift',
+        "f3": 'f3',
+        "f2": 'f2',
+        "1": '1',
+        "2": '2',
+        "3": '3',
+        "4": '4',
+        "5": '5',
+        "6": '6',
+        "7": '7',
+        "8": '8',
+        "9": '9'
+    }
+
+    TURN_MAPPINGS = {
+        "turn left": (-100, 0),
+        "turn right": (100, 0),
+        "turn up": (0, -100),
+        "turn down": (0, 100)
+    }
 
     @staticmethod
     def is_valid_message(message):
         return message in Controller.ALLOWED_MESSAGES
 
     @staticmethod
+    def is_minecraft_active():
+        try:
+            active_win = gw.getActiveWindow()
+            if active_win and "Minecraft" in active_win.title:
+                return True
+            else:
+                return False
+        except Exception:
+            return False
+
+    @staticmethod
     def perform_action(command):
+        if not Controller.is_minecraft_active():
+            print(f"{Fore.RED}Minecraft is not the active window. No action performed.{Fore.RESET}")
+            return
+
         print(f"{Fore.CYAN}Attempting to perform action: {command}{Fore.RESET}")
+
+        if command == "dance":
+            for _ in range(5):  # Repeat 5 times
+                pyautogui.keyDown('shift')  # Shift
+                time.sleep(0.3)
+                pyautogui.keyUp('shift')
+
+                pyautogui.keyDown(',')  # Attack
+                time.sleep(0.3)
+                pyautogui.keyUp(',')
+
+                pyautogui.moveRel(0, -100, duration=0.2)  # Turn up
+            print(f"{Fore.GREEN}Performed dance sequence!{Fore.RESET}")
+            return
 
         if command.startswith("turn"):
             Controller.turn_head(command)
@@ -28,32 +88,7 @@ class Controller:
             print(f"{Fore.RED}Unknown or disallowed command: {command}{Fore.RESET}")
             return
 
-        key_mappings = {
-            "w": 'w',
-            "a": 'a',
-            "s": 's',
-            "d": 'd',
-            "space": 'space',
-            "attack": ',',
-            "open": '.',
-            "place": '.',
-            "mine": ',',
-            "e": 'e',
-            "toggle sprint": 'shift',
-            "f3": 'f3',
-            "f2": 'f2',
-            "1": '1',
-            "2": '2',
-            "3": '3',
-            "4": '4',
-            "5": '5',
-            "6": '6',
-            "7": '7',
-            "8": '8',
-            "9": '9'
-        }
-
-        key = key_mappings.get(command)
+        key = Controller.KEY_MAPPINGS.get(command)
         if key:
             pyautogui.keyDown(key)
             if command == "mine":
@@ -67,13 +102,7 @@ class Controller:
 
     @staticmethod
     def turn_head(command):
-        turn_mappings = {
-            "turn left": (-100, 0),
-            "turn right": (100, 0),
-            "turn up": (0, -100),
-            "turn down": (0, 100)
-        }
-
-        dx, dy = turn_mappings.get(command, (0, 0))
+        dx, dy = Controller.TURN_MAPPINGS.get(command, (0, 0))
         pyautogui.moveRel(dx, dy, duration=0.2)
         print(f"{Fore.YELLOW}Turned head {command.split(' ')[1]}{Fore.RESET}")
+
