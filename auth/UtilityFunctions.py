fe7444a342b91fc92cc33491d534392eae039d69
Person98 <jacesleeman603@gmail.com>
2023-08-16 18:40:48 -0400

    updated rqwuerhjijdment

--- auth/UtilityFunctions.py
+++ auth/UtilityFunctions.py
@@ -1,8 +1,10 @@
 import pickle
 
+
 def save_auth_details(platform, auth_details):
     with open(f"{platform}_auth.pkl", 'wb') as f:
         pickle.dump(auth_details, f)
+
 
 def load_auth_details(platform):
     try:
